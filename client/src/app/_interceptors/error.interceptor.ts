37108d23a4f902860988a358eaca85d65f9575d6
sdas12472003 <supanta.das1247@gmail.com>
2025-05-01 21:37:18 +0530

    End of section 9

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -31,7 +31,7 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
             }
             break;
           case 401:
-            toastr.error('Unauthorised', error.status)
+            toastr.error('Unauthorized', error.status)
             break;
           case 404:
             router.navigateByUrl('/not-found');
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,20 +1,18 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { RegisterComponent } from '../register/register.component';
-import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-home',
   imports: [RegisterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit{
-  http=inject(HttpClient);
-  registerMode=false;
-  users:any;
+export class HomeComponent{
 
-  ngOnInit():void{
-    this.getUsers();
-  }
+  registerMode=false;
+
+
+  
   registerToggle()
   {
     this.registerMode=!this.registerMode;
@@ -23,12 +21,5 @@ export class HomeComponent implements OnInit{
   {
     this.registerMode=event;
   }
-  getUsers()
-  {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response =>this.users=response ,
-      error:error =>console.log(error),
-      complete:()=>console.log('Request has completed')
-     })
-  }
+  
 }
