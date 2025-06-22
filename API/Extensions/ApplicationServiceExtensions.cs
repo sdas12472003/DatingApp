dbec7cbba6437d334a1ed99aab03d456e06a063b
sdas12472003 <supanta.das1247@gmail.com>
2025-06-22 16:36:51 +0530

    Clean up: Added .gitignore and removed local DB files from tracking

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -11,22 +11,26 @@ public static class ApplicationServiceExtensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
     {
         services.AddControllers();
+
+        // ✅ Use SQLite instead of SQL Server
         services.AddDbContext<DataContext>(opt =>
         {
-            opt.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
         });
+
         services.AddCors();
         services.AddScoped<ITokenService, TokenService>();
-        services.AddScoped<IUserRepository,UserRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<ILikesRepository, LikesRepository>();
         services.AddScoped<IMessageRepository, MessageRepository>();
         services.AddScoped<IUnitOfWork, UnitOfWork>();
-        services.AddScoped<IPhotoService,PhotoService>();
+        services.AddScoped<IPhotoService, PhotoService>();
         services.AddScoped<LogUserActivity>();
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
         services.AddSignalR();
         services.AddSingleton<PresenceTracker>();
+
         return services;
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,57 @@
 
+// using API;
+// using API.Data;
+// using API.Entities;
+// using API.Extensions;
+// using API.SignalR;
+// using Microsoft.AspNetCore.Builder;
+// using Microsoft.AspNetCore.Identity;
+// using Microsoft.EntityFrameworkCore;
+
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Add services to the container.
+// builder.Services.AddApplicationServices(builder.Configuration);
+// builder.Services.AddIdentityServices(builder.Configuration);
+// var app = builder.Build();
+
+// // Configure the HTTP request pipeline.
+// app.UseMiddleware<ExceptionMiddleware>();
+// app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
+// .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
+
+// app.UseAuthentication();
+// app.UseAuthorization();
+
+// app.UseDefaultFiles();
+// app.UseStaticFiles();
+
+
+// app.MapControllers();
+// app.MapHub<PresenceHub>("hubs/presence");
+// app.MapHub<MessageHub>("hubs/message");
+// app.MapFallbackToController("Index", "Fallback");
+
+
+// using var scope = app.Services.CreateScope();
+// var services=scope.ServiceProvider;
+// try
+// {
+//     var context=services.GetRequiredService<DataContext>();
+//     var userManager = services.GetRequiredService<UserManager<AppUser>>();
+//     var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
+//     await context.Database.MigrateAsync();
+//     await context.Database.ExecuteSqlRawAsync("DELETE FROM [Connections]");
+//     // await context.Database.ExecuteSqlRawAsync("DELETE FROM \"Connections\"");
+//     await Seed.SeedUsers(userManager, roleManager);
+// }
+// catch (Exception ex)
+// {
+//     var logger =services.GetRequiredService<ILogger<Program>>();
+//     logger.LogError(ex,"An error occurred during migration");
+// }
+// app.Run();
 using API;
 using API.Data;
 using API.Entities;
@@ -10,16 +63,20 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 app.UseMiddleware<ExceptionMiddleware>();
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
-.WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
+app.UseCors(x => x
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.UseAuthentication();
 app.UseAuthorization();
@@ -27,28 +84,39 @@ app.UseAuthorization();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
 app.MapFallbackToController("Index", "Fallback");
 
-
+// Apply migrations and seed data
 using var scope = app.Services.CreateScope();
-var services=scope.ServiceProvider;
+var services = scope.ServiceProvider;
+
 try
 {
-    var context=services.GetRequiredService<DataContext>();
+    var context = services.GetRequiredService<DataContext>();
     var userManager = services.GetRequiredService<UserManager<AppUser>>();
     var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
+
     await context.Database.MigrateAsync();
-    await context.Database.ExecuteSqlRawAsync("DELETE FROM [Connections]");
-    // await context.Database.ExecuteSqlRawAsync("DELETE FROM \"Connections\"");
+
+    // SQLite doesn't support square brackets in table names
+    if (context.Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+    {
+        await context.Database.ExecuteSqlRawAsync("DELETE FROM Connections");
+    }
+    else
+    {
+        await context.Database.ExecuteSqlRawAsync("DELETE FROM [Connections]");
+    }
+
     await Seed.SeedUsers(userManager, roleManager);
 }
 catch (Exception ex)
 {
-    var logger =services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex,"An error occurred during migration");
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
 }
+
 app.Run();
