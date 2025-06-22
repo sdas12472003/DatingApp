ae8fc9b1c43a1f92649dd748a0c16acb210edd97
sdas12472003 <supanta.das1247@gmail.com>
2025-06-22 19:49:55 +0530

    fix: added deployed client URL to CORS policy

--- API/Program.cs
+++ API/Program.cs
@@ -76,7 +76,9 @@ app.UseCors(x => x
     .AllowAnyHeader()
     .AllowAnyMethod()
     .AllowCredentials()
-    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+    // .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+    .WithOrigins("https://datingapp-2i22.onrender.com", "http://localhost:4200", "https://localhost:4200"));
+
 
 app.UseAuthentication();
 app.UseAuthorization();
