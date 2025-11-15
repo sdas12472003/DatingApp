6a4a947628326e88455540e7c5b01c449b1b71bd
Supanta Das <supanta.das1247@gmail.com>
2025-11-15 13:47:40 +0530

    Update Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -31,9 +31,9 @@ app.UseCors(x => x
 );
 
 
-app.UseMiddleware<ExceptionMiddleware>();
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
-.WithOrigins("http://localhost:4200", "https://localhost:4200"));
+// app.UseMiddleware<ExceptionMiddleware>();
+// app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
+// .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.UseAuthentication();
 app.UseAuthorization();
