9c92f232a209431a7ca0b42841ea4f8675b74bb3
sdas12472003 <supanta.das1247@gmail.com>
2025-11-15 13:11:40 +0530

    Allow CORS for deployed frontend origin

--- API/Program.cs
+++ API/Program.cs
@@ -17,8 +17,18 @@ var app = builder.Build();
 
 
 app.UseMiddleware<ExceptionMiddleware>();
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
-.WithOrigins("http://localhost:4200", "https://localhost:4200","https://dating-app-f8z6.onrender.com"));
+// app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
+// .WithOrigins("http://localhost:4200", "https://localhost:4200","https://dating-app-f8z6.onrender.com"));
+app.UseCors(x => x
+    .WithOrigins(
+        "http://localhost:4200",
+        "https://localhost:4200",
+        "https://dating-app-f8z6.onrender.com"   // <-- add your frontend URL exactly
+    )
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+);
 
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
