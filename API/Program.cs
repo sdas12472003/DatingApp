f80256d13c191a009d20cbfee63646e69c9374fe
sdas12472003 <supanta.das1247@gmail.com>
2025-11-15 11:53:36 +0530

    ok

--- API/Program.cs
+++ API/Program.cs
@@ -15,7 +15,6 @@ builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
 .WithOrigins("http://localhost:4200", "https://localhost:4200"));
